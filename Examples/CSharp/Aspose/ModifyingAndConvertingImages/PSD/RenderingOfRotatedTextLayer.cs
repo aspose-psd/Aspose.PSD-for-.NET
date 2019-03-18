730c25329bca785e91f09294eb414b4e5280449c
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-03-18 23:14:21 +0500

    Added Aspose.PSD for .NET 19.3 examples

---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/RenderingOfRotatedTextLayer.cs
@@ -0,0 +1,37 @@
+﻿using Aspose.PSD.FileFormats.Png;
+using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
+{
+    class RenderingOfRotatedTextLayer
+    {
+        public static void Run() {
+            //ExStart:RenderingOfRotatedTextLayer
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSD();
+
+            string sourceFileName = dataDir + "TransformedText.psd";
+            string exportPath = dataDir + "TransformedTextExport.psd";
+            string exportPathPng = dataDir + "TransformedTextExport.png";
+            var im = (PsdImage)Image.Load(sourceFileName);
+            using (im)
+            {
+                im.Save(exportPath);
+                im.Save(exportPathPng, new PngOptions()
+                {
+                    ColorType = PngColorType.Grayscale
+                });
+            }
+            //ExEnd:RenderingOfRotatedTextLayer
+
+
+        }
+    }
+}
---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/StrokeEffectWithColorFill .cs
@@ -0,0 +1,53 @@
+﻿using Aspose.PSD.FileFormats.Png;
+using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Psd.Layers.FillSettings;
+using Aspose.PSD.FileFormats.Psd.Layers.LayerEffects;
+using Aspose.PSD.ImageLoadOptions;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
+{
+    class StrokeEffectWithColorFill
+    {
+        public static void Run() {
+
+            //ExStart:StrokeEffectWithColorFill
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSD();
+
+            // Implement rendering of Stroke effect with Color Fill for export
+            string sourceFileName = dataDir + "StrokeComplex.psd";
+            string exportPath = dataDir + "StrokeComplexRendering.psd";
+            string exportPathPng = dataDir+"StrokeComplexRendering.png";
+
+            var loadOptions = new PsdLoadOptions()
+            {
+                LoadEffectsResource = true
+            };
+
+            using (var im = (PsdImage)Image.Load(sourceFileName, loadOptions))
+            {
+                for (int i = 0; i < im.Layers.Length; i++)
+                {
+                    var effect = (StrokeEffect)im.Layers[i].BlendingOptions.Effects[0];
+                    var settings = (ColorFillSettings)effect.FillSettings;
+                    settings.Color = Color.DeepPink;
+                }
+
+                // Save psd
+                im.Save(exportPath, new PsdOptions());
+
+                // Save png
+                im.Save(exportPathPng, new PngOptions() { ColorType = PngColorType.TruecolorWithAlpha });
+            }
+
+            //ExEnd:StrokeEffectWithColorFill
+        }
+    }
+}
