f312958472503cd8b688833b05551f8b2576e273
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-07-20 00:15:31 +0500

    Added examples w.r.t 19.6

---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSB/PSBToJPG.cs
@@ -0,0 +1,42 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.ImageLoadOptions;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSB
+{
+    class PSBToJPG
+    {
+        public static void Run() {
+
+            //ExStart:PSBToJPG
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSB();
+
+            string[] sourceFileNames = new string[] { 
+               //Test files with layers
+               "Little",
+               "Simple",
+               //Test files without layers
+               "psb",
+               "psb3"
+           };
+            var options = new PsdLoadOptions();
+            foreach (var fileName in sourceFileNames)
+            {
+                var sourceFileName = dataDir + fileName + ".psb";
+                using (PsdImage image = (PsdImage)Image.Load(sourceFileName, options))
+                {
+                    // All jpeg and psd files must be readable
+                    image.Save(dataDir + fileName + "_output.jpg", new JpegOptions() { Quality = 95 });
+                    image.Save(dataDir +  fileName + "_output.psb");
+                }
+            }
+            //ExEnd:PSBToJPG
+        }
+    }
+}
---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSB/PSBToPSD.cs
@@ -0,0 +1,29 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSB
+{
+    class PSBToPSD
+    {
+        public static void Run() {
+
+            //ExStart:PSBToPSD
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSB();
+
+            string sourceFilePathPsb = dataDir + "2layers.psb";
+            string outputFilePathPsd = dataDir +  "ConvertFromPsb.psd";
+            using (Image img = Image.Load(sourceFilePathPsb))
+            {
+                var options = new PsdOptions((PsdImage)img) { FileFormatVersion = FileFormatVersion.Psd };
+                img.Save(outputFilePathPsd, options);
+            }
+            //ExEnd:PSBToPSD
+        }
+    }
+}
