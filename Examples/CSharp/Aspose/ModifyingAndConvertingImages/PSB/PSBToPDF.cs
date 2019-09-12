f032f6d8cf4c530626ffa9cc51a4a9bf09ab6c61
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-09-12 23:21:45 +0500

    Added example w.r.t 19.8

---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSB/PSBToPDF.cs
@@ -0,0 +1,29 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSB
+{
+    class PSBToPDF
+    {
+        public static void Run() {
+
+            //ExStart:PSBToPDF
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSB();
+
+            string sourceFileName = dataDir + "Simple.psb";
+            using (PsdImage image = (PsdImage)Image.Load(sourceFileName))
+            {
+                string outFileName = dataDir + "Simple.pdf";
+                image.Save(outFileName, new PdfOptions());
+            }
+            //ExEnd:PSBToPDF
+
+        }
+    }
+}
