a5fc0cc60e89ac48f11a48cfeb1353f3afb67f4e
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-09-12 23:20:49 +0500

    Added example w.r.t 19.8

---
+++ Examples/CSharp/Aspose/Opening/LoadingImageFromStream.cs
@@ -0,0 +1,61 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Psd.Layers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.Opening
+{
+    class LoadingImageFromStream
+    {
+        public static void Run() {
+
+            //ExStart:LoadingImageFromStream
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Opening();
+
+            string outputFilePath = dataDir + "PsdResult.psd";
+
+            var filesList = new string[]
+            {
+             "PsdExample.psd",
+             "BmpExample.bmp",
+             "GifExample.gif",
+             "Jpeg2000Example.jpf",
+             "JpegExample.jpg",
+             "PngExample.png",
+             "TiffExample.tif",
+            };
+            using (var image = new PsdImage(200, 200))
+            {
+                foreach (var fileName in filesList)
+                {
+                    string filePath = dataDir + fileName;
+                    using (var stream = new FileStream(filePath, FileMode.Open))
+                    {
+                        Layer layer = null;
+                        try
+                        {
+                            layer = new Layer(stream);
+                            image.AddLayer(layer);
+                        }
+                        catch (Exception e)
+                        {
+                            if (layer != null)
+                            {
+                                layer.Dispose();
+                            }
+                            throw e;
+                        }
+                    }
+                }
+                image.Save(outputFilePath);
+            }
+            //ExEnd:LoadingImageFromStream
+
+        }
+    }
+}
