5e434b4001f9804990c3bd8b3b124472d2cc12be
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-09-28 22:51:48 +0500

    Updated example w.r.t 19.9

--- Examples/CSharp/Aspose/DrawingAndFormattingImages/AdjustingBrightness.cs
+++ Examples/CSharp/Aspose/DrawingAndFormattingImages/AdjustingBrightness.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Tiff.Enums;
+using Aspose.PSD.ImageOptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Aspose.PSD.FileFormats.Psd;
-using Aspose.PSD.FileFormats.Tiff.Enums;
-using Aspose.PSD.ImageOptions;
-using Aspose.PSD.Sources;
 
 namespace Aspose.PSD.Examples.Aspose.DrawingAndFormattingImages
 {
@@ -14,40 +13,29 @@ namespace Aspose.PSD.Examples.Aspose.DrawingAndFormattingImages
     {
         public static void Run()
         {
+            //ExStart:AdjustingBrightness
+
             // The path to the documents directory.
             string dataDir = RunExamples.GetDataDir_PSD();
 
-            //ExStart:AdjustingBrightness
+            
 
-            String sourceFile = dataDir + @"sample.psd";
+            string sourceFile = dataDir + @"sample.psd";
             string destName = dataDir + @"AdjustBrightness_out.tiff";
 
-            // Load an existing image into an instance of RasterImage class
-            using (var image = Image.Load(sourceFile))
+            using (var image = (PsdImage)Image.Load(sourceFile))
             {
-                // Cast object of Image to RasterImage
-                RasterImage rasterImage = (RasterImage)image;
+                RasterCachedImage rasterImage = image;
 
-                // Check if RasterImage is cached and Cache RasterImage for better performance
-                if (!rasterImage.IsCached)
-                {
-                    rasterImage.CacheData();
-                }
-
-                // Adjust the brightness
+                // Set the brightness value. The accepted values of brightness are in the range [-255, 255].
                 rasterImage.AdjustBrightness(-50);
 
-                // Create an instance of TiffOptions for the resultant image, Set various properties for the object of TiffOptions and Save the resultant image
                 TiffOptions tiffOptions = new TiffOptions(TiffExpectedFormat.Default);
-                tiffOptions.BitsPerSample = new ushort[] { 8, 8, 8 };
-                tiffOptions.Photometric = TiffPhotometrics.Rgb;
-
-                rasterImage.Save(destName, tiffOptions);
+                rasterImage.Save(destName,tiffOptions);
             }
 
             //ExEnd:AdjustingBrightness
 
         }
-
     }
 }
