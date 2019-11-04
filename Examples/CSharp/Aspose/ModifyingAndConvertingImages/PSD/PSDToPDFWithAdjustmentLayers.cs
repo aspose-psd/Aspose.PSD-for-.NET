eeddea5dd211ae61b428621779fcae82671974e3
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-11-04 10:42:16 +0500

    Added examples w.r.t 19.10

---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/PSDToPDFWithAdjustmentLayers.cs
@@ -0,0 +1,30 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
+{
+    class PSDToPDFWithAdjustmentLayers
+    {
+        public static void Run()
+        {
+
+            //ExStart:PSDToPDFWithAdjustmentLayers
+            // Add color overlay layer effect at runtime
+            string dataDir = RunExamples.GetDataDir_PSD();
+
+
+            using (PsdImage image = (PsdImage)Image.Load(dataDir + "example.psd"))
+            {
+                image.Save(dataDir+ "document.pdf", new PdfOptions());
+            }
+            //ExEnd:PSDToPDFWithAdjustmentLayers
+
+
+        }
+    }
+}
---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/PSDToPDFWithClippingMask.cs
@@ -0,0 +1,30 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
+{
+    class PSDToPDFWithClippingMask
+    {
+        public static void Run()
+        {
+
+            //ExStart:PSDToPDFWithClippingMask
+            // Add color overlay layer effect at runtime
+            string dataDir = RunExamples.GetDataDir_PSD();
+
+
+            using (PsdImage image = (PsdImage)Image.Load(dataDir + "clip.psd"))
+            {
+                image.Save(dataDir + "output.pdf", new PdfOptions());
+            }
+            //ExEnd:PSDToPDFWithClippingMask
+
+
+        }
+    }
+}
