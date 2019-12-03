fb4f0321fb1e0992ef30d949bed6a1a48661dea2
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-12-03 12:37:14 +0500

    Added example files w.r.t 19.11

---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/GetPropertiesOfInlineFormattingOfTextLayer.cs
@@ -0,0 +1,68 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Psd.Layers;
+using Aspose.PSD.FileFormats.Psd.Layers.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples
+{
+    class GetPropertiesOfInlineFormattingOfTextLayer
+    {
+
+        public static void Run()
+        {
+            //ExStart:GetPropertiesOfInlineFormattingOfTextLayer
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSD();
+
+            using (var psdImage = (PsdImage)Image.Load(dataDir + "inline_formatting.psd"))
+            {
+                List<ITextPortion> regularText = new List<ITextPortion>();
+                List<ITextPortion> boldText = new List<ITextPortion>();
+                List<ITextPortion> italicText = new List<ITextPortion>();
+                var layers = psdImage.Layers;
+                for (int index = 0; index < layers.Length; index++)
+                {
+                    var layer = layers[index];
+                    if (!(layer is TextLayer))
+                    {
+                        continue;
+                    }
+
+                    var textLayer = (TextLayer)layer;
+                    // gets fonts that contains in text layer
+                    var fonts = textLayer.GetFonts();
+                    var textPortions = textLayer.TextData.Items;
+
+                    foreach (var textPortion in textPortions)
+                    {
+                        TextFontInfo font = fonts[textPortion.Style.FontIndex];
+                        if (font != null)
+                        {
+                            switch (font.Style)
+                            {
+                                case FontStyle.Regular:
+                                    regularText.Add(textPortion);
+                                    break;
+                                case FontStyle.Bold:
+                                    boldText.Add(textPortion);
+                                    break;
+                                case FontStyle.Italic:
+                                    italicText.Add(textPortion);
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+                        }
+                    }
+                }
+            }
+
+            //ExEnd:GetPropertiesOfInlineFormattingOfTextLayer
+        }
+    }
+}
---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/SupportOfInnerShadowLayerEffect.cs
@@ -0,0 +1,52 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Psd.Layers.LayerEffects;
+using Aspose.PSD.ImageLoadOptions;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
+{
+    class SupportOfInnerShadowLayerEffect
+    {
+        public static void Run() {
+
+            //ExStart:SupportOfInnerShadowLayerEffect
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PSD();
+
+            string sourceFile = dataDir + "innershadow.psd";
+            string destName   = dataDir+ "innershadow_out.psd";
+
+            var loadOptions = new PsdLoadOptions()
+            {
+                LoadEffectsResource = true
+            };
+
+            // Load an existing image into an instance of PsdImage class
+            using (var image = (PsdImage)Image.Load(sourceFile, loadOptions))
+            {
+                var layer = image.Layers[image.Layers.Length - 1];
+                var shadowEffect = (IShadowEffect)layer.BlendingOptions.Effects[0];
+
+                shadowEffect.Color = Color.Green;
+                shadowEffect.Opacity = 128;
+                shadowEffect.Distance = 1;
+                shadowEffect.UseGlobalLight = false;
+                shadowEffect.Size = 2;
+                shadowEffect.Angle = 45;
+                shadowEffect.Spread = 50;
+                shadowEffect.Noise = 5;
+
+                image.Save(destName, new PsdOptions(image));
+            }
+
+            //ExEnd:SupportOfInnerShadowLayerEffect
+
+        }
+    }
+}
