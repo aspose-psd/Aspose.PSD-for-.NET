beba02e3e5679ba68a9953fc3b65c0d943a854c4
aspose-yaroslav-lisovskyi <Yaroslav.Lisovskyi@aspose.com>
2021-07-15 11:57:47 +0300

    added example for release 21.7

---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/SupportOfEditFontNameInTextPortionStyle.cs
@@ -0,0 +1,76 @@
+﻿using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Psd.Layers;
+using Aspose.PSD.FileFormats.Psd.Layers.FillLayers;
+using Aspose.PSD.FileFormats.Psd.Layers.FillSettings;
+using Aspose.PSD.FileFormats.Psd.Layers.Text;
+using Aspose.PSD.ImageOptions;
+using System;
+using System.IO;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
+{
+    class SupportOfEditFontNameInTextPortionStyle
+    {
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string outputDir = RunExamples.GetDataDir_Output();
+
+            //ExStart:SupportOfEditFontNameInTextPortionStyle
+            //ExSummary:The following code demonstrate the ability to change font name at portion style.
+
+            string outputFilePng = outputDir + "result_fontEditTest.png";
+            string outputFilePsd = outputDir + "fontEditTest.psd";
+
+            void AssertAreEqual(object expected, object actual)
+            {
+                if (!object.Equals(expected, actual))
+                {
+                    throw new Exception("Objects are not equal.");
+                }
+            }
+
+            using (var image = new PsdImage(500, 500))
+            {
+                FillLayer backgroundFillLayer = FillLayer.CreateInstance(FillType.Color);
+                ((IColorFillSettings)backgroundFillLayer.FillSettings).Color = Color.White;
+                image.AddLayer(backgroundFillLayer);
+
+                TextLayer textLayer = image.AddTextLayer("Text 1", new Rectangle(10, 35, image.Width, 35));
+
+                ITextPortion firstPortion = textLayer.TextData.Items[0];
+                firstPortion.Style.FontName = FontSettings.GetAdobeFontName("Comic Sans MS");
+
+                var secondPortion = textLayer.TextData.ProducePortion();
+                secondPortion.Text = "Text 2";
+                secondPortion.Paragraph.Apply(firstPortion.Paragraph);
+                secondPortion.Style.Apply(firstPortion.Style);
+                secondPortion.Style.FontName = FontSettings.GetAdobeFontName("Arial");
+
+                textLayer.TextData.AddPortion(secondPortion);
+                textLayer.TextData.UpdateLayerData();
+
+                image.Save(outputFilePng, new PngOptions());
+                image.Save(outputFilePsd);
+            }
+
+            using (var image = (PsdImage)Image.Load(outputFilePsd))
+            {
+                TextLayer textLayer = (TextLayer)image.Layers[2];
+
+                string adobeFontName1 = FontSettings.GetAdobeFontName("Comic Sans MS");
+                string adobeFontName2 = FontSettings.GetAdobeFontName("Arial");
+
+                AssertAreEqual(adobeFontName1, textLayer.TextData.Items[0].Style.FontName);
+                AssertAreEqual(adobeFontName2, textLayer.TextData.Items[1].Style.FontName);
+            }
+
+            //ExEnd:SupportOfEditFontNameInTextPortionStyle
+
+            Console.WriteLine("SupportOfEditFontNameInTextPortionStyle executed successfully");
+
+            File.Delete(outputFilePng);
+            File.Delete(outputFilePsd);
+        }
+    }
+}
