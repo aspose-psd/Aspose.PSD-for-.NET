28aab020dff4bfafb812d991118b3d3552331c44
aspose-yaroslav-lisovskyi <Yaroslav.Lisovskyi@aspose.com>
2021-10-15 15:24:45 +0300

    fix the place of 'MergeOnePSDlayerToOther' and 'TextLayerBoundBox' classes in files

--- Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/TextLayerBoundBox.cs
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/TextLayerBoundBox.cs
@@ -1,36 +1,42 @@
-﻿using Aspose.PSD.FileFormats.Psd;
+﻿using System;
+using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Psd.Layers;
 
 namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
 {
-    class MergeOnePSDlayerToOther
+    class TextLayerBoundBox
     {
         public static void Run()
         {
             // The path to the documents directory.
             string dataDir = RunExamples.GetDataDir_PSD();
 
-            //ExStart:MergeOnePSDlayerToOther
+            //ExStart:TextLayerBoundBox
 
-            var sourceFile1 = dataDir + "FillOpacitySample.psd";
-            var sourceFile2 = dataDir + "ThreeRegularLayersSemiTransparent.psd";
-            var exportPath = dataDir + "MergedLayersFromTwoDifferentPsd.psd";
+            string sourceFileName = dataDir + "LayerWithText.psd";
 
+            var correctOpticalSize = new Size(127, 45);
+            var correctBoundBox = new Size(172, 62);
 
-            using (var im1 = (PsdImage)(Image.Load(sourceFile1)))
+            using (var im = (PsdImage)(Image.Load(sourceFileName)))
             {
-                var layer1 = im1.Layers[1];
+                var textLayer = (TextLayer)im.Layers[1];
 
-                using (var im2 = (PsdImage)(Image.Load(sourceFile2)))
+                // Size of the layer is the size of the rendered area
+                var opticalSize = textLayer.Size;
+
+                // TextBoundBox is the maximum layer size for Text Layer. 
+                // In this area PS will try to fit your text
+                var boundBox = textLayer.TextBoundBox;
+
+                if (opticalSize != correctOpticalSize ||
+                    boundBox.Size != correctBoundBox)
                 {
-                    var layer2 = im2.Layers[0];
-
-                    layer1.MergeLayerTo(layer2);
-                    im2.Save(exportPath);
+                    throw new Exception("Assertion failed");
                 }
             }
 
-            //ExEnd:MergeOnePSDlayerToOther
-
+            //ExEnd:TextLayerBoundBox
         }
     }
 }
