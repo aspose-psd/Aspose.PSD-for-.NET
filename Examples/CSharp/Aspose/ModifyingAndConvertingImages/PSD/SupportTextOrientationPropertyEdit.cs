c55c922b2a60b45096f31142cd6b706d025c4ffd
Yaroslav Lisovsky <yaroslav.lisovskyi@aspose.com>
2022-12-13 16:47:19 +0200

    Updated Aspose.PSD reference to 22.12. Added SupportTextOrientationPropertyEdit example.

---
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/SupportTextOrientationPropertyEdit.cs
@@ -0,0 +1,62 @@
+using System;
+using System.IO;
+using Aspose.PSD.FileFormats.Psd;
+using Aspose.PSD.FileFormats.Psd.Layers;
+using Aspose.PSD.FileFormats.Psd.Layers.Animation;
+using Aspose.PSD.FileFormats.Psd.Layers.FillSettings;
+
+namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
+{
+    public class SupportTextOrientationPropertyEdit
+    {
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string baseDir = RunExamples.GetDataDir_PSD();
+            string outputDir = RunExamples.GetDataDir_Output();
+
+            //ExStart:SupportTextOrientationPropertyEdit
+            //ExSummary:The following code demonstrates the ability to edit the new TextOrientation property. This does not affect rendering at the moment, but only allows you to edit the property value.
+
+            string src = Path.Combine(baseDir, "1336test.psd");
+            string output = Path.Combine(outputDir, "out_1336test.psd");
+
+            using (var image = (PsdImage)Image.Load(src))
+            {
+                var textLayer = image.Layers[1] as TextLayer;
+                if (textLayer.TextData.TextOrientation == TextOrientation.Vertical)
+                {
+                    // Correct reading
+                }
+                else
+                {
+                    throw new Exception("Incorrect reading of TextOrientation property value");
+                }
+            
+                textLayer.TextData.TextOrientation = TextOrientation.Horizontal;
+                textLayer.TextData.UpdateLayerData();
+            
+                image.Save(output);
+            }
+            
+            using (var image = (PsdImage)Image.Load(output))
+            {
+                var textLayer = image.Layers[1] as TextLayer;
+                if (textLayer.TextData.TextOrientation == TextOrientation.Horizontal)
+                {
+                    // Correct reading
+                }
+                else
+                {
+                    throw new Exception("Incorrect reading of TextOrientation property value");
+                }
+            }
+
+            //ExEnd:SupportTextOrientationPropertyEdit
+
+            File.Delete(output);
+
+            Console.WriteLine("SupportTextOrientationPropertyEdit executed successfully");
+        }
+    }
+}
