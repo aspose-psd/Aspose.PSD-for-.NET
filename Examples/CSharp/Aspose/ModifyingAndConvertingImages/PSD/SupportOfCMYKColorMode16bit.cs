fc4d7034cfa37885817056cf47da2eeb2db073da
Yaroslav Lisovsky <yaroslav.lisovskyi@aspose.com>
2023-03-24 17:49:04 +0200

    minor fixes from Examples.cs

--- Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/SupportOfCMYKColorMode16bit.cs
+++ Examples/CSharp/Aspose/ModifyingAndConvertingImages/PSD/SupportOfCMYKColorMode16bit.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Aspose.PSD.FileFormats.Psd;
 using Aspose.PSD.ImageOptions;
 
@@ -14,7 +15,11 @@ namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
             //ExStart:SupportOfCMYKColorMode16bit
             //ExSummary:The following code demonstrates the support of the CMYK ColorMode 16 bit and the ability to drawing by using Aspose.PSD.Graphics class.
 
-            using (PsdImage image = (PsdImage)Image.Load(baseFolder + "cub16bit_cmyk.psd"))
+            string srcFile = Path.Combine(baseFolder, "cub16bit_cmyk.psd");
+            string outputPsd = Path.Combine(output, "output.psd");
+            string outputPng = Path.Combine(output, "output.png");
+
+            using (PsdImage image = (PsdImage)Image.Load(srcFile))
             {
                 RasterCachedImage raster = image.Layers[0];
                 Graphics graphics = new Graphics(raster);
@@ -22,11 +27,14 @@ namespace Aspose.PSD.Examples.Aspose.ModifyingAndConvertingImages.PSD
                 int height = raster.Height;
                 Rectangle rect = new Rectangle(width / 3, height / 3, width - (2 * (width / 3)) - 1, height - (2 * (height / 3)) - 1);
                 graphics.DrawRectangle(new Pen(Color.DarkGray, 1), rect);
-                image.Save(output + "output.psd");
-                image.Save(output + "output.png", new PngOptions());
+                image.Save(outputPsd);
+                image.Save(outputPng, new PngOptions());
             }
 
             //ExEnd:SupportOfCMYKColorMode16bit
+            
+            File.Delete(outputPsd);
+            File.Delete(outputPng);
 
             Console.WriteLine("SupportOfCMYKColorMode16bit executed successfully");
         }
